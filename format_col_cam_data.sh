3533de6df7192f464f9e4f46a7375b9880f74cec
goulustis <cnimaiv@gmail.com>
2023-01-12 21:51:05 -0800

    change rgb name format

--- format_col_cam_data.sh
+++ format_col_cam_data.sh
@@ -1,6 +1,18 @@
-IMG_DIR=/home/matthew/Videos/checker_png
-COLMAP_DIR=data/checker/checker_recon/sparse/0
-IMG_SCALE=2
-TARG_DIR=data/formatted_checkers
+# This will turn color images into a formatted dataset
 
-python format_data/format_col_set.py --img_dir $IMG_DIR --colmap_dir $COLMAP_DIR --img_scale $IMG_SCALE --target_dir $TARG_DIR
+IMG_DIR=/home/matthew/Videos/checker_png            # original images
+COLMAP_DIR=data/checker/checker_recon/sparse/0      # output of colmap
+IMG_SCALE=2                                         # scale of image used for colmap
+TARG_DIR=data/formatted_checkers                    # location to save formatted dataset
+
+python format_data/format_col_set.py --img_dir $IMG_DIR \
+                                     --colmap_dir $COLMAP_DIR \
+                                     --img_scale $IMG_SCALE \
+                                     --target_dir $TARG_DIR \
+                                     --blurry_per 0
+
+cp -r $IMG_DIR $TARG_DIR
+cd $TARG_DIR
+mv $(basname $IMG_DIR) 1x
+mkdir rgb
+mv 1x rgb
