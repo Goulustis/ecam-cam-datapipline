bd40fd45c20849d1a3cde4048911ecc05340a6c2
goulustis <cnimaiv@gmail.com>
2023-01-12 21:36:16 -0800

    fix spelling mistake

--- format_col_cam_data.sh
+++ format_col_cam_data.sh
@@ -1,6 +1,6 @@
 IMG_DIR=/home/matthew/Videos/checker_png
 COLMAP_DIR=data/checker/checker_recon/sparse/0
 IMG_SCALE=2
-TARG_DIR=data/formated_checkers
+TARG_DIR=data/formatted_checkers
 
 python format_data/format_col_set.py --img_dir $IMG_DIR --colmap_dir $COLMAP_DIR --img_scale $IMG_SCALE --target_dir $TARG_DIR
