1b9d92893ba0d459d57574f9987bd73665ef35d6
goulustis <goulusti@student.ubc.ca>
2023-01-16 00:58:58 -0800

    create event images

--- format_data/eimg_maker.py
+++ format_data/eimg_maker.py
@@ -4,7 +4,7 @@ import os
 import matplotlib.pyplot as plt
 from tqdm import tqdm
 
-def ev_to_img(evs, e_thresh=0.15):
+def ev_to_img(x, y, p, e_thresh=0.15):
     """
     input:
         evs (np.array [type (t, x, y, p)]): events such that t in [t_st, t_st + time_delta]
@@ -13,22 +13,27 @@ def ev_to_img(evs, e_thresh=0.15):
     """
     e_thresh = 0.15
     h, w = 720, 1280
-    x, y, p = evs["x"], evs["y"], evs["p"]
+    # x, y, p = evs["x"], evs["y"], evs["p"]
 
     pos_p = p==1
     neg_p = p==0
 
-    e_img = np.zeros((h,w))
+    # e_img = np.zeros((h,w), dtype=np.int32)
+    e_img = np.zeros((h,w), dtype=np.float16)
+    cnt = np.zeros((h,w), dtype=np.int32)
 
     e_img[y[pos_p], x[pos_p]] += e_thresh
     e_img[y[neg_p], x[neg_p]] -= e_thresh
+    cnt[y,x] += 1
 
-    e_img = e_img.reshape(h,w)
+    stat = (e_img.max(), e_img.min())
+    # assert e_img.max() <= 127, "event image value overflow"
+    # assert e_img.min() >= -128, "event image value underflow"
 
-
+    # e_img = e_img.astype(np.int8)
     return e_img
 
-def create_event_imgs(events, triggers, time_delta=50, create_imgs = True):
+def create_event_imgs(events, triggers, time_delta=5000, create_imgs = True):
     """
     input:
         events (np.array [type (t, x, y, p)]): events
@@ -46,41 +51,54 @@ def create_event_imgs(events, triggers, time_delta=50, create_imgs = True):
     """
 
     # number of eimg per trigger gap
-    n_eimg_per_gap = (triggers[1] - triggers[0]) // time_delta
+    n_eimg_per_gap = int((triggers[1] - triggers[0]) // time_delta)
     eimgs = []       # the event images
     eimgs_ts = []
     eimgs_ids = []   # embedding ids for each img
     trig_ids = []    # id at each trigger
 
     if events is not None:
-        ts = events["t"]
+        t = events["t"]
+        x, y, p = events["x"], events["y"], events["p"]
 
     id_cnt = 0
-    for trig_t in triggers:
-        st_t = trig_t
-        end_t = trig_t + time_delta
-        trig_ids.append(id_cnt)
+    with tqdm(total=n_eimg_per_gap*len(triggers)) as pbar:
+        # for trig_t in triggers:
+        for trig_idx in range(1, len(triggers)):
+            trig_st, trig_end = triggers[trig_idx - 1], triggers[trig_idx]
 
-        for _ in range(n_eimg_per_gap):
-            if create_imgs:
-                cond = (st_t <= ts) & (ts <= end_t)
-                img_events = events[cond]
-                eimg = ev_to_img(img_events)
-                eimgs.append(eimg)
+            if events is not None:
+                cond = (trig_st <= t) & (t <= trig_end)
+                curr_t, curr_x, curr_y, curr_p = [e[cond] for e in [t, x, y, p]]
+            
+
+            st_t = trig_st
+            end_t = trig_st + time_delta
+            trig_ids.append(id_cnt)
+
+            for _ in range(n_eimg_per_gap):
+                if create_imgs:
+                    cond = (st_t <= curr_t) & (curr_t <= end_t)
+                    # img_events = events[cond]
+                    # eimg = ev_to_img(img_events)
+                    eimg = ev_to_img(curr_x[cond], curr_y[cond], curr_p[cond])
+                    eimgs.append(eimg)
 
 
-            eimgs_ids.append(id_cnt)
-            eimgs_ts.append(st_t)
+                eimgs_ids.append(id_cnt)
+                eimgs_ts.append(st_t)
 
-            # update
-            st_t = end_t
-            end_t = end_t + time_delta
-            id_cnt += 1
+                # update
+                st_t = end_t
+                end_t = end_t + time_delta
+                id_cnt += 1
+
+                pbar.update(1)
 
     if create_imgs:
-        return np.stack(eimgs), np.array(eimgs_ts), np.array(eimgs_ids), np.array(trig_ids)
+        return np.stack(eimgs), np.array(eimgs_ts, dtype=np.int32), np.array(eimgs_ids, dtype=np.int32), np.array(trig_ids, dtype=np.int32)
     else:
-        return None, np.array(eimgs_ts), np.array(eimgs_ids), np.array(trig_ids)
+        return None, np.array(eimgs_ts, dtype=np.int32), np.array(eimgs_ids, dtype=np.int32), np.array(trig_ids, dtype=np.int32)
 
 
 
