5d3ae5d4c5745f490d04ca87d56d2d171b5c035e
goulustis <goulusti@student.ubc.ca>
2023-01-17 20:38:52 -0800

    rescaling tools

---
+++ rescaling/rescale_eimgs.py
@@ -0,0 +1,40 @@
+import numpy as np
+import cv2
+import os.path as osp
+import os 
+from tqdm import tqdm 
+import argparse
+
+"""
+assume eimgs is at 1x scale
+"""
+
+def main(eimg_path, scale):
+    print("loading eimgs")
+    eimgs = np.load(eimg_path)
+    print("dtype", eimgs.dtype)
+
+    targ_path = osp.join(osp.dirname(eimg_path), f"eimgs_{scale}x")
+
+    n, h, w = eimgs.shape[:3]
+    new_h, new_w = h//scale, w//scale
+
+    re_eimgs = np.zeros((n, new_h, new_w), dtype=eimgs.dtype)
+
+    for i, eimg in tqdm(enumerate(eimgs), total=len(eimgs), desc="resize eimgs"):
+        eimg = eimg.astype(np.float32)
+        re_img = cv2.resize(eimg,(new_w, new_h), interpolation=cv2.INTER_AREA)
+        eimg = eimg.astype(np.float16)
+
+        re_eimgs[i] = re_img
+    
+    np.save(targ_path, re_eimgs)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--eimg_path", default="data/formatted_checker/ecam_set/eimgs/eimgs_1x.npy")
+    parser.add_argument("--scale", type=float, default=2)
+    args = parser.parse_args()
+
+    main(args.eimg_path, args.scale)
---
+++ rescaling/rescale_rgb.py
@@ -0,0 +1,38 @@
+import cv2
+from tqdm import tqdm
+import glob
+import os.path as osp
+import os 
+import argparse
+from tqdm import tqdm
+
+def main(img_dir, scale = 2):
+    """
+    assume img_dir contains original size images
+    
+    inputs:
+        img_dir (str): path to image dir
+        scale (int): scale to reduce image size by
+    """
+    targ_dir = osp.join(osp.dirname(img_dir), f'{scale}x')
+    os.makedirs(targ_dir, exist_ok=True)    
+
+    img_fs = sorted(glob.glob(osp.join(img_dir, "*")))
+    h, w = cv2.imread(img_fs[0]).shape[:2]
+    new_h, new_w = h//2, w//2
+
+    for f in tqdm(img_fs, desc="resizing rgb"):
+        img = cv2.imread(f)
+        re_img = cv2.resize(img, (new_w, new_h), interpolation=cv2.INTER_AREA)
+
+        save_path = osp.join(targ_dir, osp.basename(f))
+        cv2.imwrite(save_path, re_img)
+    
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--img_dir", default="data/formatted_checker/colcam_set/rgb/1x")
+    parser.add_argument("--scale", type=float, default=2)
+    args = parser.parse_args()
+
+    main(args.img_dir, args.scale)
