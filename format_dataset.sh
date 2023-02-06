f746d706d686f27852959f3a31311372c1f82f82
goulustis <goulusti@student.ubc.ca>
2023-02-05 22:21:40 -0800

    add option to not create eimgs

--- format_dataset.sh
+++ format_dataset.sh
@@ -1,20 +1,24 @@
 # code to format the data into a dataset usuable by algorithm
 # if using the structure detailed in README. Only [REQUIRED] ones are needed to be put in
+source ~/.bashrc
+conda activate ecam_proc
 
 WORKING_DIR=$(pwd)
 ##################### general config for both dataset ############################
-SCENE_PATH=data/checker                  # [REQUIRED] path/scene
-TARG_DIR=data/formatted_checkers         # [REQUIRED] path to location to save the formatted dataset
-RELCAM_PATH=$SCENE_PATH/rel_cam.json     # path to relative camera
-TRIGGER_PATH=$SCENE_PATH/triggers.txt     # path to triggers
-ECAM_TARG_DIR=$TARG_DIR/ecam_set         # path to save processed event camera data
+SCENE_PATH=data/rgb_checker                  # [REQUIRED] path/scene
+TARG_DIR=data/formatted_rgb_checker          # [REQUIRED] path to location to save the formatted dataset
+CREATE_EIMG=False                            # [REQUIRED] True for creating event images, False for just the camera extrinsics
+RELCAM_PATH=$SCENE_PATH/rel_cam.json         # path to relative camera
+TRIGGER_PATH=$SCENE_PATH/triggers.txt        # path to triggers
+ECAM_TARG_DIR=$TARG_DIR/ecam_set             # path to save processed event camera data
 
 echo formatting event camera dataset
 
 python format_data/format_ecam_set.py --scene_path $SCENE_PATH \
                                       --relcam_path $RELCAM_PATH \
                                       --targ_dir $ECAM_TARG_DIR \
-                                      --trigger_path $TRIGGER_PATH
+                                      --trigger_path $TRIGGER_PATH \
+                                      --create_eimgs $CREATE_EIMG
 
 
 
@@ -25,7 +29,7 @@ COLMAP_PATH=$SCENE_PATH/${SCENE}_recon/sparse/0    # path to sparse colmap outpu
 COLCAM_TARG_DIR=$TARG_DIR/colcam_set               # path to save save the color camera data
 TRIG_IDS_PATH=$ECAM_TARG_DIR/trig_ids.npy          # path to trigger ids when formatting event camera set
 BLURRY_PER=0                                       # [REQUIRED] parameter to filter blurry images or not; 0 to turn off
-IMG_SCALE=2                                        # [REQUIRED] scale of used on colmap (eg if used 2x downsampled of original images, then this is 2)
+IMG_SCALE=1                                        # [REQUIRED] scale of used on colmap (eg if used 2x downsampled of original images, then this is 2)
 
 
 echo formating color camera dataset
