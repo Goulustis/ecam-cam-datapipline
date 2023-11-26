0afe2ad1906fc1c7b6e8d1535d74c9e9f4be5c48
goulustis <goulusti@student.ubc.ca>
2023-11-26 02:07:31 -0800

    choose correct directory

--- extrinsics_visualization/run_dense.sh
+++ extrinsics_visualization/run_dense.sh
@@ -1,8 +1,10 @@
 #!/.bashrc
+# NOTE: MUST BE RUNNED IN extrinsics_visualization directory
 conda activate nstudio
 
 WORKDIR=/scratch-ssd/dense_workdir
-COLMAP_DIR=/ubc/cs/research/kmyi/matthew/backup_copy/raw_real_ednerf_data/Videos/book_sofa_recons
+# COLMAP_DIR=/ubc/cs/research/kmyi/matthew/backup_copy/raw_real_ednerf_data/Videos/book_sofa_recons
+COLMAP_DIR=/ubc/cs/research/kmyi/matthew/backup_copy/raw_real_ednerf_data/work_dir/sofa_soccer_dragon
 
 WORK_COLMAP=$WORKDIR/$(basename $COLMAP_DIR)
 
@@ -49,6 +51,10 @@ colmap stereo_fusion \
 mkdir $WORK_COLMAP/dense/txt_sparse
 echo $WORK_COLMAP/dense/sparse
 colmap model_converter \
-    --input_path $WORK_COLMAP/dense/sparse/0 \
+    --input_path $WORK_COLMAP/dense/sparse \
     --output_path $WORK_COLMAP/dense/txt_sparse \
     --output_type TXT
+
+# ffmpeg -framerate 24 -i vid_frames/%04d.png -c:v libx264 -pix_fmt yuv420p video.mp4
+# ffmpeg -framerate 24 -i vid_frames/%04d.png -c:v libx264 -pix_fmt yuv420p -frames:v 5000 video.mp4
+# ffmpeg -framerate 30 -i sofa_soccer_dragon_vid_frames/%04d.png -c:v libx264 -pix_fmt yuv420p -frames:v 5000 sofa_soccer_dragon_video.mp4
