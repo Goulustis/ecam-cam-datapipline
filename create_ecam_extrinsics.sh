e58e6446ebde2ec3db82c16d313fb0cb93b2f4e0
goulustis <goulusti@student.ubc.ca>
2023-11-26 02:09:20 -0800

    moved path line

--- create_ecam_extrinsics.sh
+++ create_ecam_extrinsics.sh
@@ -1,8 +1,8 @@
 # NOTE: run find_relative_cam.sh to find rel_cam.json
 # This script will find the event camera extrinsics 
 
-REL_CAM_PATH=data/rgb_checker/rel_cam.json                         # [REQUIRED] relative camera positions
-SCENE_PATH=data/rgb_checker                                        # [REQUIRED] path to your scene (eg. some_path/scene)
+REL_CAM_PATH=/ubc/cs/research/kmyi/matthew/backup_copy/raw_real_ednerf_data/work_dir/calib_checker/rel_cam.json                         # [REQUIRED] relative camera positions
+SCENE_PATH=/ubc/cs/research/kmyi/matthew/backup_copy/raw_real_ednerf_data/work_dir/calib_checker                                       # [REQUIRED] path to your scene (eg. some_path/scene)
 SCENE=$(basename $SCENE_PATH)
 PNT_3D_PATH=$SCENE_PATH/${SCENE}_recon/sparse/0/points3D.bin   # output of colmap
 IMG_PATH=$SCENE_PATH/${SCENE}_recon/sparse/0/images.bin        # output of colmap
@@ -10,10 +10,11 @@ SCALE_PNT_PATH=$SCENE_PATH/${SCENE}_recon/scale_pnts.txt       # path containing
 REAL_SCALE_PATH=$SCENE_PATH/${SCENE}_recon/pnt_dist.txt        # path containing actual distance of 3d points in (cm, mm, ...) is 
                                                                #      consistent with unit of square for finding relative camera params
 
+COLMAP_SCALE_PATH=$SCENE_PATH/${SCENE}_recon/colmap_scale.txt
+
 ## find real scale of colmap
 ## save scale under $SCENE_PATH/$SCENE/colmap_scale.txt
 echo finding the scale
-COLMAP_SCALE_PATH=$SCENE_PATH/${SCENE}_recon/colmap_scale.txt
 python colmap_find_scale/find_scale.py -c $PNT_3D_PATH -p $SCALE_PNT_PATH -l $REAL_SCALE_PATH
 
 # CREATE THE EXTRININSICS
