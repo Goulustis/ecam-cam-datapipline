ce37a472ec4450dd55d30602910b27520421ef9c
goulustis <goulusti@student.ubc.ca>
2023-12-16 21:00:17 -0800

    concatenate_videos support vid_f, img_dir, list[nparray]

--- viz/comb_vid.py
+++ viz/comb_vid.py
@@ -1,6 +1,9 @@
+import glob
+import os.path as osp
 import cv2
 import numpy as np
 from tqdm import tqdm
+
 
 def resize_and_pad(frame, target_height, target_width, pad_color=0):
     h, w = frame.shape[:2]
@@ -16,14 +19,89 @@ def resize_and_pad(frame, target_height, target_width, pad_color=0):
 
     return padded
 
-def concatenate_videos(video_path1, video_path2, output_path):
-    cap1 = cv2.VideoCapture(video_path1)
-    cap2 = cv2.VideoCapture(video_path2)
+# def concatenate_videos(video_path1, video_path2, output_path):
+#     cap1 = cv2.VideoCapture(video_path1)
+#     cap2 = cv2.VideoCapture(video_path2)
 
-    width1 = int(cap1.get(cv2.CAP_PROP_FRAME_WIDTH))
-    height1 = int(cap1.get(cv2.CAP_PROP_FRAME_HEIGHT))
-    width2 = int(cap2.get(cv2.CAP_PROP_FRAME_WIDTH))
-    height2 = int(cap2.get(cv2.CAP_PROP_FRAME_HEIGHT))
+#     width1 = int(cap1.get(cv2.CAP_PROP_FRAME_WIDTH))
+#     height1 = int(cap1.get(cv2.CAP_PROP_FRAME_HEIGHT))
+#     width2 = int(cap2.get(cv2.CAP_PROP_FRAME_WIDTH))
+#     height2 = int(cap2.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+#     target_height = max(height1, height2)
+#     target_width = max(width1, width2)
+
+#     fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+#     # out = cv2.VideoWriter(output_path, fourcc, 16, (target_width * 2, target_height))
+#     out=None
+
+#     # Determine the total number of frames in the longer video
+#     total_frames = int(min(cap1.get(cv2.CAP_PROP_FRAME_COUNT), cap2.get(cv2.CAP_PROP_FRAME_COUNT)))
+
+#     frame_idx = 0
+#     # Using tqdm for the progress bar
+#     with tqdm(total=total_frames, desc="Processing", unit="frame") as pbar:
+#         while True:
+#             ret1, frame1 = cap1.read()
+#             ret2, frame2 = cap2.read()
+
+#             if not ret1 and not ret2:
+#                 break
+
+#             if ret1:
+#                 frame1 = resize_and_pad(frame1, target_height, target_width)
+#             else:
+#                 frame1 = np.zeros((target_height, target_width, 3), dtype=np.uint8)
+
+#             if ret2:
+#                 frame2 = resize_and_pad(frame2, target_height, target_width)
+#             else:
+#                 frame2 = np.zeros((target_height, target_width, 3), dtype=np.uint8)
+
+#             combined_frame = np.hstack((frame1, frame2))
+#             if out is None:
+#                 out = cv2.VideoWriter(output_path, fourcc, 16, combined_frame.shape[:2][::-1])
+            
+#             font = cv2.FONT_HERSHEY_SIMPLEX
+#             cv2.putText(combined_frame, f"{frame_idx}", (50, 50), font, 1, (0, 0, 255), 2, cv2.LINE_AA)
+
+#             out.write(combined_frame)
+            
+
+#             pbar.update(1)
+#             frame_idx += 1
+
+#     cap1.release()
+#     cap2.release()
+#     out.release()
+
+
+def read_vid(path):
+    if type(path) == str:
+        if ".mp4" in path:
+            cap = cv2.VideoCapture(path)
+            n_frames = cap.get(cv2.CAP_PROP_FRAME_COUNT)
+            flag, frames = True, []
+            with tqdm(total=n_frames, desc="loading frames") as pbar:
+                while flag:
+                    flag, frame = cap.read()
+                    frames.append(frame)
+                    pbar.update(1)
+        else:
+            frames = [cv2.imread(f) for f in sorted(glob.glob(osp.join(path, "*.png")))]
+    elif type(path) == list:
+        frames = path
+    else:
+        assert 0, f"{path} not supported for reading"
+
+    return frames
+
+def concatenate_videos(video_path1, video_path2, output_path):
+    cap1_ori, cap2_ori = read_vid(video_path1), read_vid(video_path2)
+
+    height1, width1 = cap1_ori[0].shape[:2]
+    height2, width2 = cap2_ori[0].shape[:2]
+    cap1, cap2 = iter(cap1_ori), iter(cap2_ori)
 
     target_height = max(height1, height2)
     target_width = max(width1, width2)
@@ -33,16 +111,19 @@ def concatenate_videos(video_path1, video_path2, output_path):
     out=None
 
     # Determine the total number of frames in the longer video
-    total_frames = int(min(cap1.get(cv2.CAP_PROP_FRAME_COUNT), cap2.get(cv2.CAP_PROP_FRAME_COUNT)))
+    total_frames = int(min(len(cap1_ori), len(cap2_ori)))
 
     frame_idx = 0
-    # Using tqdm for the progress bar
+    ret1, ret2 = True, True
     with tqdm(total=total_frames, desc="Processing", unit="frame") as pbar:
         while True:
-            ret1, frame1 = cap1.read()
-            ret2, frame2 = cap2.read()
+            # ret1, frame1 = cap1.read()
+            # ret2, frame2 = cap2.read()
+            frame1, frame2 = next(cap1), next(cap2)
 
-            if not ret1 and not ret2:
+            # if not ret1 and not ret2:
+            #     break
+            if (frame1 is None) or (frame2 is None):
                 break
 
             if ret1:
@@ -68,12 +149,15 @@ def concatenate_videos(video_path1, video_path2, output_path):
             pbar.update(1)
             frame_idx += 1
 
-    cap1.release()
-    cap2.release()
+    if len(cap1_ori) != len(cap2_ori):
+        print("\033[31m" + f"WARNING: the video size are not the same!! vid1:{len(cap1_ori)}, vid2: {len(cap2_ori)}" + "\033[0m")
+
+    # cap1.release()
+    # cap2.release()
     out.release()
 
 
 # Example usage
-concatenate_videos('/scratch/matthew/projects/ecam-cam-datapipline/tmp/black_seoul_b1_v3_trig_ecamset.mp4', 
-                   '/scratch/matthew/projects/ecam-cam-datapipline/tmp/black_seoul_b1_v3_colcam_set.mp4', 
-                   '/scratch/matthew/projects/ecam-cam-datapipline/tmp/comb_vid.mp4')
+concatenate_videos('/scratch/matthew/projects/ecam-cam-datapipline/tmp/black_seoul_b3_v3_trig_ecamset.mp4', 
+                   '/scratch/matthew/projects/ecam-cam-datapipline/tmp/black_seoul_b3_v3_colcam_set.mp4', 
+                   '/scratch/matthew/projects/ecam-cam-datapipline/tmp/debug.mp4')
