5b08b23a869409bb88dc26edf212b6b874878bc5
goulustis <goulusti@student.ubc.ca>
2024-02-06 17:45:32 -0800

    add colcam_set manager

--- extrinsics_visualization/colmap_scene_manager.py
+++ extrinsics_visualization/colmap_scene_manager.py
@@ -7,6 +7,7 @@ import colmap_find_scale.read_write_model as colmap_utils
 import collections
 from dataclasses import dataclass
 import numpy as np
+from nerfies.camera import Camera
 
 
 
@@ -121,7 +122,7 @@ def concat_imgs(img1, img2):
     return concatenated_image
 
 
-class Camera:
+class ColmapCamera:
 
     def __init__(self, cam_f):
         self.col_cam = colmap_utils.read_cameras_binary(cam_f)[1]
@@ -137,7 +138,7 @@ class Camera:
     def get_dist_coeffs(self):
         return np.array([self.k1, self.k2, self.p1, self.p2])
 
-class ColSceneManager:
+class ColmapSceneManager:
     """
     retrieves corresponding 3d points and plot them in 3d
     """
@@ -153,7 +154,7 @@ class ColSceneManager:
         self.img_fs = sorted(glob.glob(osp.join(self.img_dir, "*.png")))
         self.images = colmap_utils.read_images_binary(osp.join(self.colmap_dir, "sparse/0/images.bin"))
         self.pnts_3d = colmap_utils.read_points3D_binary(osp.join(self.colmap_dir,"sparse/0","points3D.bin"))
-        self.camera = Camera(osp.join(self.colmap_dir, "sparse/0", "cameras.bin"))
+        self.camera = ColmapCamera(osp.join(self.colmap_dir, "sparse/0", "cameras.bin"))
         self.chosen_points = None
         self.pnts_2d=None
 
@@ -306,9 +307,46 @@ class ColSceneManager:
         img_f = self.get_img_f(img_idx)
         return osp.basename(img_f).split(".")[0]
 
+class ColcamSetManager:
+
+    def __init__(self, colcam_set_dir):
+        self.colcam_set_dir = colcam_set_dir
+        self.img_fs = sorted(glob.glob(osp.join(colcam_set_dir, "rgb", "1x", "*.png")))
+        self.cam_fs = sorted(glob.glob(osp.join(colcam_set_dir, "camera", "*.json")))
+
+        self.ref_cam = Camera.from_json(self.cam_fs[0])
+
+
+    def get_img_f(self,idx):
+        return self.img_fs[idx]
+
+    def get_extrnxs(self, idx):
+        extrxs_f = self.cam_fs[idx]
+        cam = Camera.from_json(extrxs_f)
+        R = cam.orientation
+        T = -R@cam.position
+        T = T.reshape(3,1)
+
+        return np.concatenate([R, T], axis=1)
+
+    def get_intrnxs(self):
+        """
+        return K, distortions
+        """
+        fx = fy = self.ref_cam.focal_length
+        cx, cy = self.ref_cam.principal_point_x, self.ref_cam.principal_point_y
+        k1, k2, k3 = self.ref_cam.radial_distortion
+        p1, p2 = self.ref_cam.tangential_distortion
+
+        intrx_mtx = np.array([[fx, 0, cx],
+                              [0, fy, cy],
+                              [0, 0, 1]])
+        dist = np.array((k1, k2, p1, p2))
+
+        return intrx_mtx, dist
 
 if __name__ == "__main__":
     colmap_dir = "/ubc/cs/research/kmyi/matthew/backup_copy/raw_real_ednerf_data/Videos/atrium_b2_v1_recons"
-    manager = ColSceneManager(colmap_dir)
+    manager = ColmapSceneManager(colmap_dir)
     manager.view_img_points(3)
 
