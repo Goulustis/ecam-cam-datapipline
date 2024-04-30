7cb095c710d02a2d7acb6970f39625057e25fcfa
goulustis <goulusti@student.ubc.ca>
2024-04-29 17:17:23 -0700

    use type

--- format_data/format_e2nerf.py
+++ format_data/format_e2nerf.py
@@ -50,7 +50,7 @@ def undistort_and_save_img(colmap_dir, save_dir, cond, ret_k_only=False):
 
     im_h, im_w = cv2.imread(img_fs[0]).shape[:2]
     new_K, roi = cv2.getOptimalNewCameraMatrix(K, D, (im_w, im_h), 1, (im_w, im_h))
-    mapx, mapy = cv2.initUndistortRectifyMap(K, D, None, new_K, (im_w, im_h), 5)
+    mapx, mapy = cv2.initUndistortRectifyMap(K, D, None, new_K, (im_w, im_h), cv2.CV_32FC1)
     x, y, w, h = roi
     new_K[0, 2] -= x
     new_K[1, 2] -= y
@@ -320,13 +320,13 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--work_dir", type=str, default="")
     parser.add_argument("--targ_dir", type=str, default="")
-    parser.add_argument("--n_bins", type=int, default=6)
+    parser.add_argument("--n_bins", type=int, default=4)
     parser.add_argument("--cam_only", type=bool, default=False, help="if true, will process camera only. no events or images will be copied/processed")
     args = parser.parse_args()
 
-    main(args.work_dir, args.targ_dir, args.n_bins)
-    # main("/ubc/cs/research/kmyi/matthew/backup_copy/raw_real_ednerf_data/work_dir/atrium_b2_v1",
-    #     #  "/ubc/cs/research/kmyi/matthew/projects/E2NeRF/data/real-world/atrium_finer",
-    #      "debug",
-    #      args.n_bins,
-    #      cam_only=False)
+    # main(args.work_dir, args.targ_dir, args.n_bins)
+    main("/ubc/cs/research/kmyi/matthew/backup_copy/raw_real_ednerf_data/work_dir/atrium_b2_v1",
+        #  "/ubc/cs/research/kmyi/matthew/projects/E2NeRF/data/real-world/atrium_finer",
+         "debug",
+         args.n_bins,
+         cam_only=False)
