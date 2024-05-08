cb202b9376d1df1faad2ebe2023457d928dfe20c
goulustis <goulusti@student.ubc.ca>
2024-05-07 22:08:19 -0700

    add functionality to manager

--- stereo_calib/data_scene_manager.py
+++ stereo_calib/data_scene_manager.py
@@ -44,12 +44,15 @@ class ColcamSceneManager:
         with open(self.dataset_json_f, "r") as f:
             self.dataset = json.load(f)
             self.img_ids = [int(e) for e in self.dataset["ids"]]
+            self.train_ids = self.dataset["train_ids"]
+            self.val_ids = self.dataset["val_ids"]
     
 
         self.img_fs = [self.img_fs[e] for e in self.img_ids]
 
         self.cam_fs = sorted(glob.glob(osp.join(self.data_dir, "camera", "*.json")))
         self.img_shape = self.get_img(0).shape[:2]
+        self.ts = self.load_ts(self.cam_fs)
     
     def get_img(self, idx):
         return cv2.imread(self.img_fs[idx])
@@ -57,6 +60,14 @@ class ColcamSceneManager:
     def get_intrnxs(self):
         return load_json_intr(self.cam_fs[0])
     
+    def load_ts(self, cam_fs):
+        ts = []
+        for cam_f in cam_fs:
+            with open(cam_f, "r") as f:
+                data = json.load(f)
+                ts.append(data["t"])
+        
+        return np.array(ts)
 
     def get_extrnxs(self, idx):
         """
@@ -76,6 +87,15 @@ class EcamSceneManager(ColcamSceneManager):
         self.data_dir = data_dir
         self.cam_fs = sorted(glob.glob(osp.join(self.data_dir, "camera", "*.json")))
         self.eimgs = np.load(osp.join(self.data_dir, "eimgs", "eimgs_1x.npy"), "r")
+
+        prev_cam_dir = osp.join(self.data_dir, "prev_camera")
+        if osp.exists(prev_cam_dir):
+            self.prev_cam_fs = sorted(glob.glob(osp.join(prev_cam_dir, "*.json")))
+            self.next_cam_fs = sorted(glob.glob(osp.join(self.data_dir, "next_camera", "*.json")))
+            self.prev_ts = self.load_ts(self.prev_cam_fs)
+            self.next_ts = self.load_ts(self.next_cam_fs)
+        
+        self.ts = self.load_ts(self.cam_fs)  # this one is slightly meaningless
 
     def __len__(self):
         return min(len(self.cam_fs), len(self.eimgs))
