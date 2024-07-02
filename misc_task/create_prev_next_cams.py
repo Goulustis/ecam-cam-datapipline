febdded5e70911e7e444660352fdb8e81594831a
goulustis <goulusti@student.ubc.ca>
2024-07-02 16:54:45 -0700

    add functionality

--- misc_task/create_prev_next_cams.py
+++ misc_task/create_prev_next_cams.py
@@ -3,6 +3,7 @@ import os.path as osp
 import glob
 import shutil
 from tqdm import tqdm
+import json 
 
 def create_prev_next_cams(cam_dir):
     prev_cam_dir = osp.join(osp.dirname(cam_dir), "prev_camera")
@@ -26,6 +27,28 @@ def create_prev_next_cams(cam_dir):
         shutil.copy(next_cam_f, next_cam_f_dst)
 
 
+def update_train_ids(scene_dir):
+    colcam_dir = osp.join(scene_dir, "colcam_set")
+    dataset_f = osp.join(colcam_dir, "dataset.json")
+
+    with open(dataset_f, "r") as f:
+        dataset = json.load(f)
+    
+    train_ids = dataset["train_ids"]
+    train_ids = sorted(list(set(train_ids) - set(dataset["val_ids"])))
+    dataset["num_exemplars"] = len(train_ids)
+    with open(dataset_f, "w") as f:
+        dataset["train_ids"] = train_ids
+        json.dump(dataset, f, indent=2)
+
+
+
 if __name__ == "__main__":
+    # scene_dir = "/ubc/cs/research/kmyi/matthew/projects/ed-nerf/data/halloween_b2_v1_rect"
+    # update_train_ids(scene_dir)
+
+    # scene_dir = "/ubc/cs/research/kmyi/matthew/projects/ed-nerf/data/atrium_b2_v1_rect"
+    # update_train_ids(scene_dir)
+
     cam_dir = "/ubc/cs/research/kmyi/matthew/projects/ed-nerf/data/depth_var_1_lr_000000/ecam_set/camera"
     create_prev_next_cams(cam_dir)
